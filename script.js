7d23ce0098419fe69207158f5920217c252733ee
ByanHasyifPulingga <ciellyan135@gmail.com>
2024-11-04 18:03:11 +0700

    script.js

--- script.js
+++ script.js
@@ -2,32 +2,28 @@ const canvas = document.getElementById('animationCanvas');
 const ctx = canvas.getContext('2d');
 
 const circle = {
-    x: 20,    // Initial x position
+    x: 20,
     y: canvas.height / 2,
     radius: 10,
-    speed: 2  // Adjust the speed as needed
+    speed: 2
 };
 
 function drawCircle() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-    // Draw the circle
     ctx.beginPath();
     ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
     ctx.fillStyle = '#fff';
     ctx.fill();
     ctx.closePath();
 
-    // Update the circle's position
     circle.x += circle.speed;
 
-    // Reset position if it moves out of the canvas on the right side
     if (circle.x - circle.radius > canvas.width) {
-        circle.x = -circle.radius; // Start from the left side again
+        circle.x = -circle.radius;
     }
 
     requestAnimationFrame(drawCircle);
 }
 
-// Start the animation
 drawCircle();
